3168add18c741b1a33879d967e9982b625f907d5
Frederick Practice1212pm <frederickpractice1212pm@myfakeemail.com>
2022-05-03 16:56:07 -0400

    node

---
+++ app.js
@@ -0,0 +1,17 @@
+module.exports =
+ [{  albumNumber: '0',
+    name:'Bad Bunny',
+    publishDate:'2004',
+    imgURL:"https://ichef.bbci.co.uk/news/976/cpsprodpb/18357/production/_121995199_gettyimages-1172292629.jpg",
+   songTitles:['song1”,“song2", “song3”, “song4']
+ },
+ {
+    albumNumber:'1',
+    name:'Legend',
+    description:'2015',
+    imgURL:"https://i.pinimg.com/originals/f8/6e/8f/f86e8f5581cd8ac92a7437dabd17b9dc.jpg",
+   songTitles:['song1','song2', 'song3', 'song4']
+ }
+];
+ 
+
